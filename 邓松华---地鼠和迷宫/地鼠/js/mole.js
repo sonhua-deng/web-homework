557cd177269ac48be41015504b16dad6e11b01e4
sonhua-deng <dengshnl@gmail.com>
2016-10-16 20:58:58 +0800

    fifth homework

---
+++ 邓松华---地鼠和迷宫/地鼠/js/mole.js
@@ -0,0 +1,97 @@
+﻿var time = 30;//时间
+var score = 0;//分数
+var bool_start_game=0;//游戏是否开始
+var bool_start = 0;//是否暂停
+var num = 0; // 生成地鼠的洞号码
+
+//生成60个洞
+for (var i = 0; i < 60; i++) {
+    var game = document.getElementById("game");//游戏主框函数
+    var button_one = document.createElement("button");
+    game.appendChild(button_one);
+    document.getElementById("game").appendChild(button_one);
+    var id_name = i;
+    button_one.setAttribute("class", "hole");
+    button_one.setAttribute("id", id_name);
+}
+
+var hole_array = document.getElementsByClassName("hole"); //hole 的数组
+var time_interval// 时间间隔
+
+//游戏开始的函数
+document.getElementById("start_stop").onclick = function () {
+    if (bool_start_game == 0) {
+        score = 0;
+        time = 30;
+        document.getElementById("score").firstChild.nodeValue = score;
+        document.getElementById("time").firstChild.nodeValue = time;
+        createMouse();
+    }
+    if (bool_start == 0) {
+        if (time == 0) {
+            score = 0;
+            time = 30;
+        }
+        bool_start = 1;
+        bool_start_game = 1;
+        time_interval = setInterval("time_change()", 1000);
+        document.getElementById("over").firstChild.nodeValue = "Playing";
+    }
+    else{
+        bool_start = 0;
+        clearInterval(time_interval);
+        document.getElementById("over").firstChild.nodeValue = "Stop";
+    }
+
+}
+
+//生成地鼠的函数
+function createMouse() {
+    var random_num = Math.round(Math.random() * 59);
+    hole_array[random_num].style.backgroundColor = "blue";
+    num = random_num;
+}
+
+//点击地鼠的函数
+for (var i = 0; i < hole_array.length; i++) {
+    var hole_now = hole_array[i];
+    hole_now.onclick = function () {
+        if (bool_start == 1&&bool_start_game==1) {
+            if (time > 0) {
+                really(this.getAttribute("id"));
+            }
+        }
+    }
+}
+
+//时间减的函数
+function time_change() {
+    if (time > 0) {
+        time = time - 1;
+        document.getElementById("time").firstChild.nodeValue = time;
+    }
+    else {
+        clearInterval(time_interval);
+        document.getElementById("over").firstChild.nodeValue = "Gameover";
+        bool_start_game = 0;
+        bool_start = 0;
+        hole_array[num].style.backgroundColor = "white";
+        alert("Score:" + score);
+    }
+}
+
+//判断是否打中地鼠，分数变化的函数
+function really(flag) {
+    if (flag == num) {
+        hole_array[num].style.backgroundColor = "white";
+        score = score + 1;
+        document.getElementById("score").firstChild.nodeValue = score;
+        createMouse();
+    }
+    else{
+        if (score > 0) {
+            score = score - 1;
+        }
+        document.getElementById("score").firstChild.nodeValue = score;
+    }
+}
---
+++ 邓松华---地鼠和迷宫/迷宫/js/maze.js
@@ -0,0 +1,109 @@
+﻿var bool_s = 0;//判断s是否被触摸过
+var bool_maze = 0;//判断鼠标是否在迷宫中
+var bool_wall = 0;//是否碰过墙；
+var bool_win = 0;//是否胜利
+var bool_cheat = 0;//有没有作弊
+
+//改变结果的函数
+function change_result() {
+    var result = document.getElementById("result");
+    result.style.marginLeft = "0px";
+    if (bool_cheat == 1) {
+        bool_cheat = 0;
+        result.style.marginLeft = "-120px";
+        result.firstChild.nodeValue = "Don't cheat, you should start form the 'S' and move to the 'E' inside the maze!";
+    }
+    else if (bool_wall == 1) {
+        result.style.marginLeft = "210px";
+        result.firstChild.nodeValue = "You  Lose";
+    }
+    else if (bool_win == 1) {
+        result.style.marginLeft = "210px";
+        result.firstChild.nodeValue = "You Win";
+    }
+    else if (bool_win == 0) {
+        result.firstChild.nodeValue = " ";
+    }
+}
+
+//鼠标在主框上
+document.getElementById("game").onmouseover = function () {
+    bool_maze = 1;
+}
+
+//鼠标在s上
+document.getElementById("s").onmouseover = function () {
+    if (bool_wall == 0&&bool_win==0&&bool_cheat==0) {
+        bool_s = 1;
+    }
+    if (bool_cheat == 0 && bool_wall==0&&bool_win == 0)
+    change_result();
+}
+
+var bar = document.getElementsByClassName("bar");//class 为bar的元素节点组
+
+//鼠标碰墙
+for (var i = 0; i < bar.length; i++) {
+    var bar_single = bar[i];
+    if (i != 4 && i != 6) {
+        bar_single.onmouseover = function () {
+            if (bool_s == 1 && bool_wall == 0) {
+                bool_wall = 1;
+                bool_s = 0;
+                this.style.backgroundColor = "red";
+                change_result();
+            }
+        }
+    }
+    else {
+        bar_single.onmouseover = function () {
+            if (bool_s == 1 && bool_wall == 0) {
+                bool_wall = 1;
+                bool_s = 0;
+                bar[4].style.backgroundColor = "red";
+                bar[6].style.backgroundColor = "red";
+                change_result();
+            }
+        }
+    }
+}
+
+//鼠标到达E
+document.getElementById("e").onmouseover = function () {
+    if (bool_cheat == 1) {               //cheat
+        bool_win = 0;
+        bool_s = 0;
+        change_result();
+        return;
+    }
+    else if (bool_s == 1 && bool_wall == 0 && bool_win == 0 && bool_cheat == 0) {     //win
+        bool_win = 1;
+        bool_s = 0;
+        change_result();
+        return;
+    }
+    else if (bool_s == 0 && bool_wall == 0 && bool_win == 0 && bool_cheat == 0) {   //cheat
+        bool_win = 0;
+        bool_s = 0;
+        bool_cheat = 1;
+        change_result();
+    }
+}
+
+//鼠标离开游戏主框
+document.getElementById("game").onmouseleave = function () {
+    if (bool_s == 1 && bool_wall == 0 && bool_win == 0) {
+        bool_cheat = 1;
+    }
+    else {
+        bool_cheat = 0;
+    }
+    if (bool_wall == 1) {
+        for (var i = 0; i < bar.length; i++) {
+            bar[i].style.backgroundColor = "#EEEEEE";
+        }
+    }
+    bool_maze = 0;
+    bool_wall = 0;
+    bool_win = 0;
+}
